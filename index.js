62e95257624d353c89bed7e51ac45258e39ef8af
Ricky Miller <ricky.miller@gmail.com>
2016-04-06 12:15:47 -0400

    Preserve case

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const vowels = 'aeiouAEIOU'
+const vowels = 'aeiou'
 
-const lolify = module.exports = function (str) {
-  const rvowels = vowels.split('').sort(function () {
+const shuffle = function (str) {
+  return str.split('').sort(function () {
     return 0.5 - Math.random()
   })
+}
 
+const replaceLetters = function (str, replaceStr) {
+  const rvowels = shuffle(replaceStr)
   return [].map.call(str, function (l) {
-    const index = vowels.indexOf(l)
+    const index = replaceStr.indexOf(l)
     return (index !== -1) ? rvowels[ index ] : l
   }).join('')
+}
+
+const lolify = module.exports = function (str) {
+  return replaceLetters(replaceLetters(str, vowels), vowels.toUpperCase())
 }
